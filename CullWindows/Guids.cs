561792b3e8500622784c712ad7478db9d8baf1e2
brunomlopes <brunomlopes@09180236-9811-11dd-9929-29c1920dd198>
2008-10-12 03:55:56 +0000

    Initial import. Kinda works. No clue on how to make it "usable"

---
+++ CullWindows/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace BrunoMLopes.CullWindows
+{
+    static class GuidList
+    {
+        public const string guidCullWindowsPkgString = "86b3430a-9294-4115-a3cd-907f077d8eaf";
+        public const string guidCullWindowsCmdSetString = "81b30c38-2906-4ede-9a19-979c9e4cda61";
+
+        public static readonly Guid guidCullWindowsCmdSet = new Guid(guidCullWindowsCmdSetString);
+        public const string guidCullWindowsDesignerOptionsPage = "BE466BF8-97FC-11DD-B095-1D6456D89593";
+    };
+}
